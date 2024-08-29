a288e2117c34cd85713a360f55218e2ae8932371
kjulieth18 <45132485+kjulieth18@users.noreply.github.com>
2024-08-28 20:05:27 -0500

    Restart position

--- Assets/ResoursersAssets/NZ PT-9M Pistol/Scripts/NzExplode.cs
+++ Assets/ResoursersAssets/NZ PT-9M Pistol/Scripts/NzExplode.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace NzBulletLookDev
+
 {
     public class NzExplode : MonoBehaviour
     {
@@ -11,51 +12,52 @@ namespace NzBulletLookDev
         public AudioClip Explode_Sound;
         public AudioClip Recall_Sound;
         public AudioSource Audio;
-    
+
         public float ExplodeSpeed;
         private bool IsExplode = false;
 
-        
-        
 
-    
+
+
+
         Vector3 pointA;
         public Vector3 pointB;
 
-        private void Start() 
+        private void Start()
         {
             pointA = transform.localPosition;
-            
+
         }
         void Update()
-         {
+        {
 
             if (Input.GetKeyDown(KeyCode.Space) && !IsExplode)
             {
-                Audio.PlayOneShot(Explode_Sound); 
+                Audio.PlayOneShot(Explode_Sound);
                 StartCoroutine(MoveObject(transform, pointA, pointB, ExplodeSpeed));
                 IsExplode = true;
             }
             else if (Input.GetKeyDown(KeyCode.Space) && IsExplode)
             {
-                Audio.PlayOneShot(Recall_Sound); 
+                Audio.PlayOneShot(Recall_Sound);
                 StartCoroutine(MoveObject(transform, pointB, pointA, ExplodeSpeed));
                 IsExplode = false;
-            
+
 
             }
-         }
-      
-         IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
-         {
-             var i= 0.0f;
-             var rate= 1.0f/time;
-             while (i < 1.0f) {
-                 i += Time.deltaTime * rate;
-                 thisTransform.localPosition = Vector3.Lerp(startPos, endPos, i);
-                 yield return null; 
-             }
-         }
-     
+        }
+
+        IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+        {
+            var i = 0.0f;
+            var rate = 1.0f / time;
+            while (i < 1.0f)
+            {
+                i += Time.deltaTime * rate;
+                thisTransform.localPosition = Vector3.Lerp(startPos, endPos, i);
+                yield return null;
+            }
+        }
+
     }
 }
